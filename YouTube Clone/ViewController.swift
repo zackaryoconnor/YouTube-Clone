d46b9883d73c60c358e05bcac91a36165f2a27fc
Zackary O'Connor <zackaryoconnor@Zackarys-iMac.local>
2020-03-19 19:04:35 -0400

    Initial commit

---
+++ YouTube Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube Clone
+//
+//  Created by Zackary O'Connor on 3/19/20.
+//  Copyright © 2020 Zackary O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
